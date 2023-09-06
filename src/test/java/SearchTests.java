86191f43cde655e290c3f66fce4886faeb9b7a22
Julia Marchenko <yuliamarche@gmail.com>
2023-09-06 22:10:33 +0300

    Update SearchTests.java

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -8,7 +8,7 @@ public class SearchTests {
     @Test
     void successfulSearchTest() {
         open("https://www.google.com/");
-        $("[name=q]").setValue("selenide").pressEnter();
-        $("[id=search]").shouldHave(text("https://ru.selenide.org"));
+        $("[name=q]").setValue("seleni").pressEnter();
+        $("[id=search]").shouldHave(text("https://selenide.org"));
     }
 }
